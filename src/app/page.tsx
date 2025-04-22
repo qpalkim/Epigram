e0dec553295aef0ea155d6fe16fc2b4678610474
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2025-04-22 13:55:10 +0900

    feat/1/공통컴포넌트 버튼 (#2)
    * ✨ feat : 라이브러리 실치 및 버튼 컴포넌트 구현
    
    * 🔧 chore : 아이콘 이미지 파일 추가
    
    * ✨ feat : 버튼 스토리북 구현
    
    * 🔧 chore : 랜딩 페이지 불필요한 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <div>랜딩 페이지</div>
-      <div className="text-2xl text-sub-blue-600">프리텐다드</div>
-      <div className="font-iropke text-2xl text-black-950">이롭게 바탕체</div>
-      <div className="font-iropke text-lg text-illust-purple">
-        이롭게 바탕체
-      </div>
-    </div>
-  );
+  return <div>랜딩 페이지</div>;
 }
