3213bf59d3730c374780b24ce1d938c2c6138a38
qpalkim <ghs0199@naver.com>
2025-04-25 12:55:52 +0900

    💄 design : framer-motion 최초 1회 애니메이션 로직 제거

--- src/app/(with-header)/(landing)/_components/MainSection.tsx
+++ src/app/(with-header)/(landing)/_components/MainSection.tsx
@@ -22,7 +22,6 @@ const motionProps = {
   initial: { opacity: 0, y: 50 },
   whileInView: { opacity: 1, y: 0 },
   transition: { duration: 0.6, ease: "easeOut" },
-  viewport: { once: true },
 };
 
 type SectionContentProps = {
