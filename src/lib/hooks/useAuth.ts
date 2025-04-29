f92425dde6304971b25db0c1bcf68bb24af4aec4
qpalkim <ghs0199@naver.com>
2025-04-29 14:17:55 +0900

    ✨ feat : auth 커스텀 훅 파일 추가

---
+++ src/lib/hooks/useAuth.ts
@@ -0,0 +1,60 @@
+import { useMutation } from "@tanstack/react-query";
+import {
+  LoginRequest,
+  AuthResponse,
+  RefreshTokenResponse,
+  SignupRequest,
+  OauthRequest,
+  OauthAppRequest,
+  OAuthAppResponse,
+} from "../types/auth";
+import {
+  login,
+  oauthApps,
+  oauthLogin,
+  refreshToken,
+  signup,
+} from "../apis/auth";
+
+// 회원가입 훅
+export const useSignup = () => {
+  return useMutation({
+    mutationFn: (params: SignupRequest) => signup(params),
+  });
+};
+
+// 로그인 훅
+export const useLogin = () => {
+  return useMutation<AuthResponse, Error, LoginRequest>({
+    mutationFn: login,
+    onError: (error) => {
+      console.error("Login error:", error);
+    },
+  });
+};
+
+// 토큰 갱신 훅
+export const useRefreshToken = () => {
+  return useMutation<RefreshTokenResponse, Error, void>({
+    mutationFn: refreshToken,
+    onError: (error) => {
+      console.error("Token refresh error:", error);
+    },
+  });
+};
+
+// 간편 로그인 App 등록/수정 훅
+export const useOauthApps = (params: OauthAppRequest) => {
+  return useMutation<OAuthAppResponse, Error, OauthAppRequest>({
+    mutationFn: () => oauthApps(params),
+  });
+};
+
+// 간편 로그인 훅
+export const useOauthLogin = (provider: "google" | "naver" | "kakao") => {
+  return useMutation<AuthResponse, Error, OauthRequest>({
+    mutationFn: async (data: OauthRequest) => {
+      return oauthLogin(provider, data);
+    },
+  });
+};
