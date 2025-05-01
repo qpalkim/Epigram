936b111f9e22649923aed8dba3bb2d9ca9c17a8a
qpalkim <ghs0199@naver.com>
2025-05-01 11:42:40 +0900

    🐛 fix : 로딩 처리 동시에 나오는 버그 개선

--- src/app/(with-header)/epigrams/_components/TodayEpigram.tsx
+++ src/app/(with-header)/epigrams/_components/TodayEpigram.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import EpigramItem from "@/components/EpigramItem";
 import { useEpigramToday } from "@/lib/hooks/useEpigram";
 
@@ -11,18 +10,19 @@ export default function TodayEpigram() {
       <h2 className="text-black-600 font-semibold text-lg lg:text-xl mb-6 lg:mb-10">
         오늘의 에피그램
       </h2>
-      {isLoading && <p>로딩 중...</p>}
-      {isError && <p>에러가 발생했어요.</p>}
-      {!data ? (
+      {isLoading ? (
+        <p>로딩 중...</p>
+      ) : isError ? (
+        <p>에러가 발생했습니다.</p>
+      ) : !data ? (
         <p className="text-blue-400 text-center">오늘의 에피그램이 없습니다.</p>
       ) : (
-        <Link href={`/epigrams/${data.id}`}>
-          <EpigramItem
-            content={data.content}
-            author={data.author}
-            tags={data.tags}
-          />
-        </Link>
+        <EpigramItem
+          content={data.content}
+          author={data.author}
+          tags={data.tags}
+          id={data.id}
+        />
       )}
     </>
   );
