8e04f740b0fa9f80955cb41535019a303b93b9b2
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2025-05-02 20:54:13 +0900

    feat/26/공통컴포넌트 프로필 이미지 (#27)
    * 🔧 chore : 기본 프로필 이미지 파일 변경
    
    * ✨ feat : 프로필 이미지 컴포넌트 구현
    
    * ✨ feat : 프로필 이미지 컴포넌트 스토리북 구현
    
    * ⚡️ style : 스토리북 control 탭 설정

---
+++ src/stories/ProfileImage.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ProfileImage from "@/components/ProfileImage";
+
+const meta: Meta<typeof ProfileImage> = {
+  title: "ProfileImage",
+  component: ProfileImage,
+  tags: ["autodocs"],
+  argTypes: {
+    src: { control: "text" },
+    size: { control: "radio", options: ["small", "medium", "large"] },
+    onClick: { action: "clicked" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ProfileImage>;
+
+export const Small: Story = {
+  args: {
+    size: "small",
+    clickable: false,
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: "medium",
+    clickable: false,
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "large",
+    clickable: true,
+  },
+};
