30ea57f6b8e3e65cddf61ed47653684b9938f022
qpalkim <ghs0199@naver.com>
2025-05-02 12:32:44 +0900

    ✨ feat : 최신 에피그램 컴포넌트 구현

--- src/app/(with-header)/epigrams/page.tsx
+++ src/app/(with-header)/epigrams/page.tsx
@@ -1,9 +1,10 @@
 import TodayEpigram from "./_components/TodayEpigram";
 import EmotionLogs from "@/components/EmotionLogs";
+import LatestEpigram from "./_components/LatestEpigram";
 
 export default function Page() {
   return (
-    <div className="max-w-[640px] mx-auto mt-[32px] lg:mt-[120px] px-6 md:px-0">
+    <div className="max-w-[640px] mx-auto mt-[32px] lg:mt-[120px] px-6 md:px-0 mb-24">
       <h2 className="text-black-600 font-semibold text-lg lg:text-xl mb-6 lg:mb-10">
         오늘의 에피그램
       </h2>
@@ -12,6 +13,10 @@ export default function Page() {
         오늘의 감정은 어떤가요?
       </h2>
       <EmotionLogs />
+      <h2 className="text-black-600 font-semibold text-lg lg:text-xl mt-14 lg:mt-35 mb-6 lg:mb-10">
+        최신 에피그램
+      </h2>
+      <LatestEpigram />
     </div>
   );
 }
