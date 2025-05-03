5091984490dd501871ab6af0ad5e472a1af5f3b1
qpalkim <ghs0199@naver.com>
2025-05-03 13:44:12 +0900

    💄 design : 스타일 코드 수정

--- src/components/LoggedOutHeader.tsx
+++ src/components/LoggedOutHeader.tsx
@@ -11,38 +11,36 @@ export default function LoggedOutHeader() {
   const isAuthPage = pathname == "/login" || pathname === "/signup";
 
   return (
-    <>
-      <header className="fixed w-full bg-blue-100 border-b border-gray-100 z-50">
-        <div className="relative px-4 max-w-[1480px] mx-auto h-[52px] md:h-[70px] lg:h-[80px] flex items-center">
-          {!isAuthPage && (
-            <Link href="/search" className="absolute left-4">
-              <Image
-                src={search}
-                alt="검색 아이콘"
-                className="w-[20px] lg:w-[28px]"
-              />
-            </Link>
-          )}
-
-          <Link href="/" className="absolute left-1/2 -translate-x-1/2">
+    <header className="fixed w-full bg-blue-100 border-b border-gray-100 z-50">
+      <div className="relative px-4 md:px-6 max-w-[1480px] mx-auto h-[52px] md:h-[70px] lg:h-[80px] flex items-center">
+        {!isAuthPage && (
+          <Link href="/search" className="absolute left-4">
             <Image
-              src={logoSm}
-              alt="로고"
-              className="w-[101px] h-[26px] lg:w-[132px] lg:h-[36px]"
+              src={search}
+              alt="검색 아이콘"
+              className="w-[20px] lg:w-[28px]"
             />
           </Link>
+        )}
 
-          {!isAuthPage && (
-            <Link href="/mypage" className="absolute right-4">
-              <Image
-                src={user}
-                alt="유저 아이콘"
-                className="w-[20px] lg:w-[28px]"
-              />
-            </Link>
-          )}
-        </div>
-      </header>
-    </>
+        <Link href="/" className="absolute left-1/2 -translate-x-1/2">
+          <Image
+            src={logoSm}
+            alt="로고"
+            className="w-[101px] h-[26px] lg:w-[132px] lg:h-[36px]"
+          />
+        </Link>
+
+        {!isAuthPage && (
+          <Link href="/mypage" className="absolute right-4">
+            <Image
+              src={user}
+              alt="유저 아이콘"
+              className="w-[20px] lg:w-[28px]"
+            />
+          </Link>
+        )}
+      </div>
+    </header>
   );
 }
