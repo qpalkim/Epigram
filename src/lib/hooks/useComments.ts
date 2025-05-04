d221419598e8d3087724e9ed948d2da0595f6826
qpalkim <ghs0199@naver.com>
2025-05-04 12:35:01 +0900

    ✨ feat : comments 커스텀 훅 제작

---
+++ src/lib/hooks/useComments.ts
@@ -0,0 +1,55 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  CommentListResponse,
+  CreateCommentRequest,
+  CreateCommentResponse,
+  GetCommentListParams,
+  UpdateCommentRequest,
+} from "../types/comments";
+import {
+  createComment,
+  deleteComment,
+  getCommentList,
+  updateComment,
+} from "../apis/comments";
+
+// 댓글 작성 훅
+export const useCreateComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation<CreateCommentResponse, unknown, CreateCommentRequest>({
+    mutationFn: createComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments"] });
+    },
+  });
+};
+
+// 댓글 목록 조회 훅
+export const useCommentList = (params: GetCommentListParams) => {
+  return useQuery<CommentListResponse>({
+    queryKey: ["comments", params],
+    queryFn: () => getCommentList(params),
+  });
+};
+
+// 댓글 수정 훅
+export const useUpdateComment = (id: number) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateCommentRequest) => updateComment(id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments"] });
+    },
+  });
+};
+
+// 댓글 삭제 훅
+export const useDeleteComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (id: number) => deleteComment(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments"] });
+    },
+  });
+};
