45f5aea93fb980ec47281c1d4ee1aeab41b52232
qpalkim <ghs0199@naver.com>
2025-05-04 10:25:52 +0900

    💄 design : 피드 페이지 스타일 수정

--- src/components/EpigramItem.tsx
+++ src/components/EpigramItem.tsx
@@ -25,8 +25,11 @@ export default function EpigramItem({
       >
         <div
           className={clsx(
-            "relative rounded-2xl h-auto p-4 md:p-6 bg-blue-100 border border-line-100 transition-transform duration-200 ease-in-out transform hover:scale-102",
-            { "lg:h-[260px] md:h-[180px] h-[140px]": isFeedPage }
+            "relative rounded-2xl p-4 md:p-6 bg-blue-100 border border-line-100 transition-transform duration-200 ease-in-out transform hover:scale-102",
+            {
+              "lg:h-[260px] md:h-[180px] h-[140px]": isFeedPage,
+              "h-auto": !isFeedPage,
+            }
           )}
           style={{ boxShadow: "0px 3px 12px 0px rgba(0, 0, 0, 0.04)" }}
         >
@@ -41,7 +44,7 @@ export default function EpigramItem({
           <div className="z-10 relative h-full flex flex-col justify-between">
             <div
               className={clsx("text-md lg:text-lg font-medium text-black-600", {
-                "line-clamp-1 md:line-clamp-3 lg:line-clamp-none break-keep overflow-hidden":
+                "line-clamp-2 md:line-clamp-3 lg:line-clamp-none break-keep overflow-hidden":
                   isFeedPage,
               })}
             >
