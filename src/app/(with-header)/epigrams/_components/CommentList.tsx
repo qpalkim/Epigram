cbaffea933620ac69ce008f1c9da5b4cb887ccfb
qpalkim <ghs0199@naver.com>
2025-05-04 19:13:05 +0900

    ✨ feat : 메인 에피그램 페이지 최신 댓글 컴포넌트 구현 및 배치

---
+++ src/app/(with-header)/epigrams/_components/CommentList.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useCommentList } from "@/lib/hooks/useComments";
+import { useMyData } from "@/lib/hooks/useUsers";
+import { UpdateCommentResponse } from "@/lib/types/comments";
+import CommentItem from "@/components/CommentItem";
+import Button from "@/components/Button";
+import Image from "next/image";
+import plus from "@/assets/icons/plus.svg";
+
+export default function CommentList() {
+  const [limit, setLimit] = useState(3);
+  const { data, isLoading, isError } = useCommentList({
+    limit: limit,
+  });
+  const { data: user } = useMyData();
+  const [commentList, setCommentList] = useState(data?.list || []);
+
+  useEffect(() => {
+    if (data?.list) {
+      setCommentList(data.list);
+    }
+  }, [data]);
+
+  const handleLoadMore = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    if ((commentList.length ?? 0) < (data?.totalCount ?? Infinity)) {
+      setLimit((prevLimit) => prevLimit + 3);
+    }
+  };
+
+  const handleUpdateComment = (updatedComment: UpdateCommentResponse) => {
+    setCommentList((prevList) =>
+      prevList.map((comment) =>
+        comment.id === updatedComment.id ? updatedComment : comment
+      )
+    );
+  };
+
+  if (isLoading) return <p>로딩 중...</p>;
+  if (isError) return <p>에러가 발생했습니다.</p>;
+
+  return (
+    <>
+      <div className="mt-10">
+        {commentList.map((comment) => (
+          <div key={comment.id}>
+            <CommentItem
+              epigramId={comment.epigramId}
+              commentId={comment.id}
+              image={comment.writer.image}
+              nickname={comment.writer.nickname}
+              content={comment.content}
+              updatedAt={comment.updatedAt}
+              isMine={user?.id === comment.writer.id}
+              onUpdate={handleUpdateComment}
+            />
+          </div>
+        ))}
+      </div>
+
+      {(commentList.length ?? 0) < (data?.totalCount ?? Infinity) && (
+        <Button
+          variant="outline"
+          size="xl"
+          isRoundedFull
+          className="mx-auto mt-14 w-[152px] lg:w-[240px]"
+          onClick={handleLoadMore}
+        >
+          <Image
+            src={plus}
+            alt="더보기 아이콘"
+            className="lg:mr-2 w-[20px] lg:w-[24px]"
+          />
+          최신 댓글 더보기
+        </Button>
+      )}
+    </>
+  );
+}
--- src/app/(with-header)/epigrams/page.tsx
+++ src/app/(with-header)/epigrams/page.tsx
@@ -1,11 +1,12 @@
 import TodayEpigram from "./_components/TodayEpigram";
 import EmotionLogs from "@/components/EmotionLogs";
 import LatestEpigram from "./_components/LatestEpigram";
+import CommentList from "./_components/CommentList";
 import FloatingButton from "@/components/FloatingButton";
 
 export default function Page() {
   return (
-    <div className="max-w-[640px] mx-auto mt-[32px] lg:mt-[120px] px-6 md:px-0 mb-24">
+    <div className="max-w-[640px] mx-auto mt-[32px] lg:mt-[120px] px-6 md:px-0 mb-32">
       <h2 className="text-black-600 font-semibold text-lg lg:text-xl mb-6 lg:mb-10">
         오늘의 에피그램
       </h2>
@@ -18,6 +19,10 @@ export default function Page() {
         최신 에피그램
       </h2>
       <LatestEpigram />
+      <h2 className="text-black-600 font-semibold text-lg lg:text-xl mt-14 lg:mt-35 mb-6 lg:mb-10">
+        최신 댓글
+      </h2>
+      <CommentList />
       <FloatingButton />
     </div>
   );
