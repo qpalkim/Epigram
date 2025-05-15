2cfd31c75cbfc263f2f3c1f4a5b7468e19b73421
qpalkim <ghs0199@naver.com>
2025-05-15 11:09:56 +0900

    🐛 fix : Suspense 적용

--- src/app/(with-header)/search/page.tsx
+++ src/app/(with-header)/search/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import SearchContainer from "./_components/SearchContainer";
 
 export default function Page() {
   return (
-    <>
+    <Suspense fallback={<div>로딩 중...</div>}>
       <SearchContainer />
-    </>
+    </Suspense>
   );
 }
