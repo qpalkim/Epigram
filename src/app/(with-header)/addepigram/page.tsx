076ab402eaaa4ef2f28a111918f552878b86282c
qpalkim <ghs0199@naver.com>
2025-05-19 12:38:17 +0900

    ✨ feat : 에피그램 작성 폼 구현

--- src/app/(with-header)/addepigram/page.tsx
+++ src/app/(with-header)/addepigram/page.tsx
@@ -1,3 +1,9 @@
+import CreateEpigramForm from "./_components/CreateEpigramForm";
+
 export default function Page() {
-  return <div>에피그램 작성 페이지</div>;
+  return (
+    <div className="max-w-[640px] mx-auto mt-12 lg:mt-20 px-6 md:px-0 mb-20">
+      <CreateEpigramForm />
+    </div>
+  );
 }
