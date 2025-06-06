713470409ec0df49bc3e92b3f1f7a0df74f1523e
qpalkim <ghs0199@naver.com>
2025-06-06 18:28:31 +0900

    ♻️ refactor : 로그인/회원가입 페이지 코드 리팩토링 및 간편 로그인 임시 제거

--- src/app/(with-header)/signup/_components/SignUpForm.tsx
+++ src/app/(with-header)/signup/_components/SignUpForm.tsx
@@ -14,14 +14,13 @@ import eye from "@/assets/icons/eye.svg";
 import eyeVisible from "@/assets/icons/eye-visible.svg";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
-import SocialLogin from "@/components/SocialLogin";
 
 export default function SignUpForm() {
   const { mutateAsync: signUp } = useSignUp();
   const { mutateAsync: login } = useLogin();
   const router = useRouter();
-  const [isShowPassword, setIsShowPassword] = useState(false);
-  const [isShowPasswordConfirm, setIsShowPasswordConfirm] = useState(false);
+  const [showPw, setShowPw] = useState(false);
+  const [showPwConfirm, setShowPwConfirm] = useState(false);
   const {
     register,
     handleSubmit,
@@ -38,8 +37,8 @@ export default function SignUpForm() {
       await signUp(data);
       const { email, password } = data;
       await login({ email, password });
-      toast.success("회원가입에 성공했습니다.");
       router.push("/epigrams");
+      toast.success("회원가입에 성공했습니다.");
     } catch (err) {
       const error = err as AxiosError;
       const status = error?.response?.status;
@@ -77,39 +76,51 @@ export default function SignUpForm() {
           <div className="relative">
             <Input
               label="비밀번호"
-              type={isShowPassword ? "text" : "password"}
+              type={showPw ? "text" : "password"}
               placeholder="비밀번호를 입력하세요"
               {...register("password", {
                 onBlur: () => trigger("password"),
               })}
               error={errors.password?.message}
             />
-            <Image
-              src={isShowPassword ? eyeVisible : eye}
-              width={24}
-              height={24}
-              alt="비밀번호 눈 아이콘"
-              onClick={() => setIsShowPassword((prev) => !prev)}
-              className="cursor-pointer absolute top-12 lg:top-15 right-4"
-            />
+            <button
+              type="button"
+              onClick={() => setShowPw(!showPw)}
+              className="cursor-pointer absolute top-12.5 lg:top-15 right-4"
+              aria-label={showPw ? "비밀번호 숨기기" : "비밀번호 보기"}
+            >
+              <Image
+                src={showPw ? eyeVisible : eye}
+                width={24}
+                height={24}
+                alt="비밀번호 아이콘"
+              />
+            </button>
           </div>
           <div className="relative">
             <Input
-              type={isShowPasswordConfirm ? "text" : "password"}
-              placeholder="비밀번호를 확인해 주세요"
+              type={showPwConfirm ? "text" : "password"}
+              placeholder="비밀번호를 한번 더 입력하세요"
               {...register("passwordConfirmation", {
                 onBlur: () => trigger("passwordConfirmation"),
               })}
               error={errors.passwordConfirmation?.message}
             />
-            <Image
-              src={isShowPasswordConfirm ? eyeVisible : eye}
-              width={24}
-              height={24}
-              alt="비밀번호 눈 아이콘"
-              onClick={() => setIsShowPasswordConfirm((prev) => !prev)}
-              className="cursor-pointer absolute top-7 lg:top-9 right-4"
-            />
+            <button
+              type="button"
+              onClick={() => setShowPwConfirm(!showPwConfirm)}
+              className="cursor-pointer absolute top-6.5 lg:top-8.5 right-4"
+              aria-label={
+                showPwConfirm ? "비밀번호 확인 숨기기" : "비밀번호 확인 보기"
+              }
+            >
+              <Image
+                src={showPwConfirm ? eyeVisible : eye}
+                width={24}
+                height={24}
+                alt="비밀번호 아이콘"
+              />
+            </button>
           </div>
         </div>
         <Input
@@ -128,13 +139,16 @@ export default function SignUpForm() {
           회원가입하기
         </Button>
       </form>
-      <p className="mt-12 text-center font-medium text-md lg:text-lg text-blue-400">
-        이미 회원이신가요?&nbsp;
-        <Link href="/login" className="underline text-black-500">
-          로그인하기
-        </Link>
-      </p>
-      <SocialLogin />
+      <div className="mt-12 flex items-center gap-4">
+        <div className="flex-grow border-t border-line-200" />
+        <p className="text-center font-medium text-md lg:text-lg text-blue-400">
+          이미 회원이신가요?&nbsp;
+          <Link href="/login" className="underline text-black-500">
+            로그인하기
+          </Link>
+        </p>
+        <div className="flex-grow border-t border-line-200" />
+      </div>
     </div>
   );
 }
