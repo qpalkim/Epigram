056c9826be22b44f953b02b403e695fc88229940
qpalkim <ghs0199@naver.com>
2025-06-06 13:19:52 +0900

    🔍 pref : 검색 엔진 최적화 작업

--- src/app/(with-header)/epigrams/[id]/page.tsx
+++ src/app/(with-header)/epigrams/[id]/page.tsx
@@ -1,19 +1,54 @@
 import { notFound } from "next/navigation";
-import EpigramDetail from "./_components/EpigramDetail";
-import EpigramComments from "./_components/EpigramComments";
+import { safeResponse } from "@/lib/network/safeResponse";
+import { epigramDetailResponseSchema } from "@/lib/types/epigrams";
+import axiosServerHelper from "@/lib/network/axiosServerHelper";
+import EpigramContainer from "./_components/EpigramContainer";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  if (isNaN(Number(id))) notFound();
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const parsedId = Number(params.id);
+  if (isNaN(parsedId)) notFound();
 
-  return (
-    <>
-      <EpigramDetail id={Number(id)} />
-      <EpigramComments id={Number(id)} />
-    </>
-  );
+  try {
+    const response = await axiosServerHelper(`/epigrams/${parsedId}`);
+    const epigramDetail = safeResponse(
+      response.data,
+      epigramDetailResponseSchema
+    );
+
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+    return {
+      title: `${epigramDetail.content.slice(0, 20)}... | Epigram`,
+      description: `“${epigramDetail.content}” - ${epigramDetail.author}`,
+      openGraph: {
+        type: "article",
+        title: `${epigramDetail.content.slice(0, 20)}... | Epigram`,
+        description: `“${epigramDetail.content}” - ${epigramDetail.author}`,
+        url: `${baseUrl}/epigrams/${parsedId}`,
+        images: [
+          {
+            url: "/thumbnail.jpg",
+            width: 800,
+            height: 600,
+            alt: "Epigram 배너",
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: `${epigramDetail.content.slice(0, 20)}... | Epigram`,
+        description: `“${epigramDetail.content}” - ${epigramDetail.author}`,
+        images: ["/thumbnail.jpg"],
+      },
+    };
+  } catch {
+    notFound();
+  }
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const parsedId = Number(params.id);
+  if (isNaN(parsedId)) notFound();
+
+  return <EpigramContainer id={parsedId} />;
 }
